bf17ad16b8f758e74735c9d68ddefa7f2153dfda
Samantha Benissa <samanthaceleridad@gmail.com>
2026-04-24 01:47:36 +0800

    PWA Completed

--- AppDev_Act14/src/index.jsx
+++ AppDev_Act14/src/index.jsx
@@ -6,3 +6,12 @@ import App from './App.jsx'
 const root = document.getElementById('root')
 
 render(() => <App />, root)
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(() => console.log("Service Worker registered"))
+      .catch((err) => console.log("SW failed:", err));
+  });
+}
